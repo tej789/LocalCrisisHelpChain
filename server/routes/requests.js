fee63c0fd30f0acf80482fb350925628df31a637
tej789 <gotitej2005@gmail.com>
2026-02-18 22:54:56 +0530

    Add nearest volunteer functionality

--- server/routes/requests.js
+++ server/routes/requests.js
@@ -1,13 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
 const requestController = require('../controllers/requestController');
+const { verifyToken, requireRole } = require('../middleware/auth');
 
-router.post('/', verifyToken, requireRole('user'),
-  requestController.createRequest);
+router.post(
+  '/',
+  verifyToken,
+  requireRole('user'),
+  requestController.createRequest
+);
 
-router.get('/', verifyToken,
-  requestController.getRequests);
+router.get(
+  '/',
+  verifyToken,
+  requestController.getRequests
+);
 
-router.put('/:id/assign', verifyToken, requireRole('ngo'),
-  requestController.assignVolunteer);
+router.put(
+  '/:id/assign',
+  verifyToken,
+  requireRole('ngo'),
+  requestController.assignVolunteer
+);
 
-router.post('/:id/resolve', verifyToken, requireRole('volunteer'),
-  requestController.resolveRequest);
+router.post(
+  '/:id/resolve',
+  verifyToken,
+  requireRole('volunteer'),
+  requestController.resolveRequest
+);
+
+module.exports = router;
