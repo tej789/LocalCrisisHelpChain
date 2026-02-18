fdda303628b950bffdce3cb27ad7493643968985
tej789 <gotitej2005@gmail.com>
2026-02-18 21:49:43 +0530

    Add nearest volunteer functionality

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,9 +4,7 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
-// attach token automatically
 api.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
   if (token) {
