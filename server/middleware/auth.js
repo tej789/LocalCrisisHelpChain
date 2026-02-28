7496ac2c7e13b046620ee02b7cb5eb6f95f14558
tej789 <gotitej2005@gmail.com>
2026-02-28 14:54:34 +0530

    Fix Admin Dashboard error

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -19,7 +19,6 @@ function verifyToken(req, res, next) {
     const decoded = jwt.verify(token, process.env.JWT_SECRET || "secretkey");
 
     req.user = { id: decoded.userId, role: decoded.role };
-
     next();
   } catch (err) {
     return res.status(401).json({ error: "Invalid or expired token" });
